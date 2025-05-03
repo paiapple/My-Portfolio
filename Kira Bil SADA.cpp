cf9a552493d1d78b475abf231c71738d76d54c25
Izzati Idris <paiapple@users.noreply.github.com>
2025-05-04 01:44:32 +0800

    Kira Bil SADA.cpp

--- Kira Bil SADA.cpp
+++ Kira Bil SADA.cpp
@@ -1,15 +1,15 @@
 // Online C++ compiler to run C++ program online
-//simply calculate SADA bill
+// simple calculate SADA bill
 #include <iostream>
 using namespace std;
 
 
 void blokTarif()
     {
-        cout << "\n\nBlok Tarif(m)     | Blok Prarata(m)          |Kadar RM";
-        cout << "\n0 - 20 (1 bulan)  : Blok Purata 1= RM20.000   Kadar = RM0.75 ";
-        cout << " \n21 - 35           : Blok Purata 2= RM15.000   Kadar = RM1.30 ";
-        cout << "\n>35               : Blok Purata 3= RM5.000    Kadar = RM1.75 ";
+        cout << "\n\nBlok Tarif(m)     | Blok Prorata(m)          |Kadar RM";
+        cout << "\n0 - 20 (1 bulan)  : Blok Prorata 1= RM20.000   Kadar = RM0.75 ";
+        cout << " \n21 - 35           : Blok Prorata 2= RM15.000   Kadar = RM1.30 ";
+        cout << "\n>35               : Blok Prorata 3= RM5.000    Kadar = RM1.75 ";
         cout << "*********************************************** ";
 }
 
@@ -34,7 +34,7 @@ int main() {
     if (meterSemasa>=meterDahulu)
     {
         totalGuna = meterSemasa - meterDahulu;
-        cout << "Jumlah kegunaan (m) bulan terkini: " << totalGuna;
+        cout << "Jumlah kegunaan(m) bulan terkini: " << totalGuna;
     }else
     {
         cout << "\ntak guna air ke? :p ";
@@ -42,7 +42,7 @@ int main() {
             
     if(totalGuna<=20)
     {
-        cout << "\n\nAmaun Blok Prarata 1: RM" <<totalGuna;
+        cout << "\n\nAmaun Blok Prorata 1: RM" <<totalGuna;
             
         float kadar1=totalGuna * 0.75;
         cout << "\nTotal Bil Sada: RM" <<kadar1;
@@ -52,12 +52,12 @@ int main() {
     {
         blokPurata1 = 20;
         float kadar1=blokPurata1 * 0.75;
-        cout << "\n\nAmaun Blok Prarata 1: RM" <<blokPurata1;
+        cout << "\n\nAmaun Blok Prorata 1: RM" <<blokPurata1;
         cout << "\nAmaun Kadar 1: RM" <<kadar1;
         
         blokPurata2 = totalGuna-blokPurata1;
         float kadar2=blokPurata2 * 1.30;
-        cout << "\nAmaun Blok Prarata 2: RM" <<blokPurata2;
+        cout << "\nAmaun Blok Prorata 2: RM" <<blokPurata2;
         cout << "\nAmaun Kadar 2: RM" <<kadar2;
                 
         float total2=kadar1+kadar2;
@@ -67,17 +67,17 @@ int main() {
     {
         blokPurata1 = 20;
         float kadar1=blokPurata1 * 0.75;
-        cout << "\n\nAmaun Blok Prarata 1: RM" <<blokPurata1;
+        cout << "\n\nAmaun Blok Prorata 1: RM" <<blokPurata1;
         cout << "\nAmaun Kadar 1: RM" <<kadar1;
         
         blokPurata2 = 15;
         float kadar2=blokPurata2 * 1.30;
-        cout << "\nAmaun Blok Prarata 2: RM" <<blokPurata2;
+        cout << "\nAmaun Blok Prorata 2: RM" <<blokPurata2;
         cout << "\nAmaun Kadar 2: RM" <<kadar2;
         
         blokPurata3 = totalGuna - 35;
         float kadar3=blokPurata3 * 1.75;
-        cout << "\nAmaun Blok Prarata 3: RM" <<blokPurata3;
+        cout << "\nAmaun Blok Prorata 3: RM" <<blokPurata3;
         cout << "\nAmaun Kadar 3: RM" <<kadar3;
                 
         float total3=kadar1+kadar2+kadar3;
